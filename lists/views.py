8d7be9413f834b6a8cefa7fb9089fd729ea42dc2
emashovvn2 <emashovvn2@sibmail.com>
2019-12-11 22:30:59 +0700

    new URL, view and template to display lists

--- lists/views.py
+++ lists/views.py
@@ -5,6 +5,11 @@ def home_page(request):
         '''домашняя страница'''
         if request.method == 'POST':
                 Item.objects.create(text=request.POST['item_text'])
-                return redirect('/')
+                return redirect('/lists/one-ones-list-in-world/')
         items = Item.objects.all()
-        return render(request, 'home.html', {'items': items})
+        return render(request, 'home.html')
+
+def view_list(request):
+        '''представление списка'''
+        items = Item.objects.all()
+        return render(request, 'list.html', {'items': items})
--- superlists/urls.py
+++ superlists/urls.py
@@ -23,4 +23,5 @@ from django.conf.urls import url
 from lists import views
 urlpatterns = [
     url(r'^$', views.home_page, name='home'),
+    url(r'^lists/one-ones-list-in-world/$', views.view_list, name='view_list'),
     ]
